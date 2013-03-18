c9a6fbad48b51141ed26ef919afadfd24656bff5
Nik Reiman <nik.reiman@gmail.com>
2013-03-18 23:43:08 +0100

    More reasonable CPU hogging
    Causes some nasty overruns on live, nice!

--- Source/ChaosCpuHog.cpp
+++ Source/ChaosCpuHog.cpp
@@ -15,9 +15,8 @@
 void ChaosCpuHog::doChaos(float* channelData, int numSamples)
 {
     double junk = 0;
-    for (int i = 0; i < numSamples * 1000; ++i) {
+    for (int i = 0; i < numSamples * 10; ++i) {
         for (int j = 0; j < numSamples; ++j) {
-            // TODO: Need to evaluate how much CPU this actually takes up on a modern CPU
             junk += log10(exp(sin(sqrt(random() % (i * j + 1)))));
         }
     }
