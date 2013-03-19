6af0f30c37211af4c59e8b5d8829f03f04c3e6ff
Nik Reiman <nik.reiman@gmail.com>
2013-03-19 08:04:52 +0100

    Hog slighly less CPU

--- Source/ChaosCpuHog.cpp
+++ Source/ChaosCpuHog.cpp
@@ -15,7 +15,7 @@
 void ChaosCpuHog::doChaos(float* channelData, int numSamples)
 {
     double junk = 0;
-    for (int i = 0; i < numSamples * 10; ++i) {
+    for (int i = 0; i < numSamples * 1; ++i) {
         for (int j = 0; j < numSamples; ++j) {
             junk += log10(exp(sin(sqrt(random() % (i * j + 1)))));
         }
